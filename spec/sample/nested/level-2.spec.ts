877ea8707e798d46426bd3bc9c20e5f89af1db14
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-05-09 11:55:31 +0100

    Example for @wakaleo

--- spec/sample/nested/level-2.spec.ts
+++ spec/sample/nested/level-2.spec.ts
@@ -3,12 +3,12 @@ import { actorCalled } from '@serenity-js/core';
 import { Navigate, Page } from '@serenity-js/web';
 import { describe, it } from 'mocha';
 
-describe('serenity-js.org website', () => {
+describe('Level 2 Feature Spec', () => {
 
     it(`tells people what they're reading`, () =>
         actorCalled('Alice').attemptsTo(
             Navigate.to('https://serenity-js.org'),
-            
+
             Ensure.that(Page.current().title(), includes('Serenity/JS')),
         ));
 });
